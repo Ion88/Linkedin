9bd5633c9a687166150b53a7e064272e00813394
Ion88 <ion.vizitiu88@gmail.com>
2020-05-15 15:04:28 +0100

    Props and State

--- react-essential-training/src/index.js
+++ react-essential-training/src/index.js
@@ -1,46 +1,68 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 
-let footballData = {
-  total: 50,
-  home: 25,
-  guest: 15,
-  goal: 100,
-};
+let bookList = [
+  { title: "Alchimist", author: "Paulo Coehlo", pages: 250 },
+  { title: "Amintiri din copilarie", author: "Ion Creanga", pages: 90 },
+  { title: "Harry Potter", author: "Nu tin minte", pages: 440 },
+];
 
-const getPercent = (decimals) => {
-  return decimals * 100 + "%";
-};
-
-const calcGoalProgress = (total, goal) => {
-  return getPercent(total / goal);
-};
-
-const FootballDayCounter = ({ total, home, guest, goal }) => {
+const Book = ({ title, author, pages, freeBookmark }) => {
   return (
     <section>
-      <div>
-        <p>Total: {total}</p>
-      </div>
-      <div>
-        <p>Home: {home}</p>
-      </div>
-      <div>
-        <p>Guest: {guest}</p>
-      </div>
-      <div>
-        <p>Goal progress: {calcGoalProgress(total, goal)}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>By: {author}</p>
+      <p>Pages: {pages} pages</p>
+      <p>Free bookmark today: {freeBookmark ? "yes" : "no"}</p>
     </section>
   );
 };
 
-render(
-  <FootballDayCounter
-    total={footballData.total}
-    home={footballData.home}
-    guest={footballData.guest}
-    goal={footballData.goal}
-  />,
-  document.getElementById("root")
+const Hiring = () => (
+  <div>
+    <p>The library is hiring. Go to www.library/jobs for more</p>
+  </div>
 );
+
+const NotHiring = () => (
+  <div>
+    <p>The library is not hiring. Check back later for more info</p>
+  </div>
+);
+
+class Library extends Component {
+  state = {
+    open: true,
+    freeBookmark: true,
+    hiring: false,
+  };
+
+  toggleOpenClosed = () => {
+    this.setState((prevState) => ({
+      open: !prevState.open,
+    }));
+  };
+
+  render() {
+    console.log(this.state);
+    const { books } = this.props;
+    return (
+      <div>
+        {this.state.hiring ? <Hiring /> : <NotHiring />}
+        <h1>The library is {this.state.open ? "open" : "closed"}</h1>
+        <button onClick={this.toggleOpenClosed}>Change</button>
+        {books.map((book, i) => (
+          <Book
+            key={i}
+            title={book.title}
+            author={book.author}
+            pages={book.pages}
+            freeBookmark={this.state.freeBookmark}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+render(<Library books={bookList} />, document.getElementById("root"));
